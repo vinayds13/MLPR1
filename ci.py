88923706aacd184bff0c52e19ac2f4c112af6b15
vinayds13 <vinayraagavendra13@gmail.com>
2024-07-18 11:08:52 +0530

    Add files via upload
    added ci file

---
+++ ci.py
@@ -0,0 +1,8 @@
+
+from urllib.parse import urlparse
+def extract_source_name(url):
+    domain = urlparse(url).netloc
+    return domain.replace('www.', '')
+
+
+print(extract_source_name('https://www.tipranks.com/news/company-announcements/tscan-therapeutics-advances-in-solid-and-hematological-cancer-therapy'))
